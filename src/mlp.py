ea1cfc0d6dacd8a3a030121ecf7edf63c300a8e9
Maximilien Pereira <maxperei@z1r5p4.42lyon.fr>
2024-08-12 18:31:48 +0200

    Add data normalization to fix neurone activation overflow

--- src/mlp.py
+++ src/mlp.py
@@ -4,6 +4,8 @@ import pandas as pd
 import sklearn.model_selection as skl_select
 from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
 
 from perceptron import Perceptron
 
@@ -14,8 +16,12 @@ from perceptron import Perceptron
 # algo optimize
 # select features
 
+
 def load_dataset(path: str) -> pd.DataFrame:
     """Load a csv dataset, name columns, drop NaN and duplicate rows
+
+    Args:
+        path (str): path to the dataset csv file to load
 
     Returns:
         pd.DataFrame: data from a csv file in a pandas dataframe
@@ -32,26 +38,34 @@ def load_dataset(path: str) -> pd.DataFrame:
 
 
 def parse_arguments() -> list[any]:
+    """Parse arguments given to the program and return the list of arguments 
+
+    Returns:
+        list[any]: list of arguments provided by the user or default values
+    """
     parser = argparse.ArgumentParser()
     
     parser.add_argument("dataset")
-    parser.add_argument("--lr", type=float, default=0.1, help="Learning rate [0.0001 - 1.0]")
+    parser.add_argument("--lr", type=float, default=0.1, help="Learning rate [0.00001 - 1.0]")
     parser.add_argument("--epochs", type=int, default=1000, help="Number of iterations [1 - 10000]")
     
     args = parser.parse_args()
     
-    args.lr = min(1.0, max(args.lr, 0.0001))
+    args.lr = min(1.0, max(args.lr, 0.00001))
     args.epochs = min(10000, max(args.epochs, 1))
     
     return args
 
 
 def fit_model(x: pd.DataFrame, y: pd.DataFrame, lr: float, epochs: int):
-    X = x.to_numpy()
+    scaler = StandardScaler()
+    X = scaler.fit_transform(x)
     Y = y.to_numpy().reshape(y.shape[0], 1).astype(float)
-    p = Perceptron(lr, len(x.columns))
+    # X, y = make_blobs(n_samples=100, n_features=2, centers=2, random_state=0)
+    # Y = y.reshape((y.shape[0], 1))
+    p = Perceptron(lr, X, Y)
     for _ in range(epochs):
-        p.train_model(X, Y)
+        p.train_model()
     plt.plot(p.cost)
     plt.show()
 
--- src/perceptron.py
+++ src/perceptron.py
@@ -1,29 +1,40 @@
 import numpy as np
 
 
-THRESHOLD = 0.5
+ACTIVATION_THRESHOLD = 0.5
 
 
 class   Perceptron:
-    def __init__(self, learning_rate: float, nb_features):
+    """Artificial neuron that takes a learning rate, and some data for training.
+    """
+    def __init__(self, learning_rate: float, X: np.ndarray, Y: np.ndarray):
         self.__learning_rate = learning_rate
-        self.__init_weights_and_bias(nb_features)
+        self.__X = X
+        self.__Y = Y
+        self.__init_weights_and_bias()
         self.__cost = []
 
-    def __init_weights_and_bias(self, nb_features):
+    def __init_weights_and_bias(self):
+        nb_features = len(self.__X[0])
         self.__W = np.random.rand(nb_features, 1)
         self.__b = np.random.randn(1)
         
-    def __compute_cost(self, Y: np.ndarray):
+    def __compute_cost(self):
+        Y = self.__Y
+        A = self.__A
         m = Y.shape[0]
-        epsilon = 0.00001
-        L = (-1 / m) * np.sum(Y * np.log(self.__A + epsilon) + (1 - Y) * np.log(1 - self.__A + epsilon))
+        epsilon = 1e-15
+        L = (-1 / m) * np.sum(Y * np.log(A + epsilon) + (1 - Y) * np.log(1 - A + epsilon))
         self.__cost.append(L) 
         
-    def __update_gradients(self, X: np.ndarray, Y: np.ndarray) -> tuple[np.ndarray, float]:
+    def __update_gradients(self) -> tuple[np.ndarray, float]:
+        X = self.__X
+        Y = self.__Y
+        A = self.__A
         m = X.shape[0]
-        dW = (1 / m) * X.T.dot(self.__A - Y)
-        db = (1 / m) * np.sum(self.__A - Y)
+        
+        dW = (1 / m) * X.T.dot(A - Y)
+        db = (1 / m) * np.sum(A - Y)
         
         self.__W -= self.__learning_rate * dW
         self.__b -= self.__learning_rate * db
@@ -33,16 +44,16 @@ class   Perceptron:
         A = 1 / (1 + np.exp(-Z))
         return A
 
-    def train_model(self, X: np.ndarray, Y: np.ndarray):
-        self.__A = self.__model(X)
-        self.__compute_cost(Y)
-        self.__update_gradients(X, Y)
+    def train_model(self):
+        self.__A = self.__model(self.__X)
+        self.__compute_cost()
+        self.__update_gradients()
         
-    def predict(self, X: np.ndarray) -> str:
-        A = self.__model(X)
-        return A >= THRESHOLD
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        A = self.__model(X_test)
+        return A >= ACTIVATION_THRESHOLD
 
-        
+
     @property
     def weights(self) -> np.ndarray:
         return self.__W
